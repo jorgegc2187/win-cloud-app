eb5162131441d267a3534141f35d7f148ebae147
Jorge Gonzales C. <jorgegc2187@gmail.com>
2024-11-22 17:34:09 -0500

    Modal agregado

--- src/veeam/pages/RepositoriesPage.jsx
+++ src/veeam/pages/RepositoriesPage.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react"
 import { RepositoriesTable } from "../components/RepositoriesTable"
+import { Modal } from "../components/Modal";
+import { RepositorieForm } from "../components/RepositorieForm";
 
 export const RepositoriesPage = () => {
+
+  const [showForm, setShowForm] = useState(false);
+  const onCloseForm = () => {
+    setShowForm(false);
+  }
+
   return (
     <>
       <h1>RepositoriesPage</h1>
-      <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center">Agregar Repositorio</button>
-      <RepositoriesTable/>
+
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center"
+        onClick={() => setShowForm(true)}
+      >
+        Agregar Repositorio
+      </button>
+
+      <Modal isVisible={ showForm } onClose={ onCloseForm } children={<RepositorieForm/>}/>
+
+      <RepositoriesTable />
 
     </>
   )
